4bc8c9a7c6287a88d7b990ecfb03eda5dd397516
lcduarte <lois_duarte@msn.com>
2018-09-09 12:42:05 +0100

    * Updates test for AndroidLayoutsApi;
    * Updates to use XsdAsm-1.0.13;

--- create_class_binaries.bat
+++ create_class_binaries.bat
@@ -1,5 +1,3 @@
-ECHO START
 if exist "./src/main/java" rmdir "./src/main/java" /s /q
 if not exist "./target/classes/org/xmlet/androidlayoutsapi" mkdir "./target/classes/org/xmlet/androidlayoutsapi"
 call mvn exec:java -D"exec.mainClass"="org.xmlet.xsdasm.main.XsdAsmMain" -D"exec.args"="./src/main/resources/android.xsd androidlayoutsapi"
-ECHO END
--- decompile_class_binaries.bat
+++ decompile_class_binaries.bat
@@ -1,5 +1,3 @@
-ECHO START
 if not exist "./src/main/java/org/xmlet/androidlayoutsapi" mkdir "./src/main/java/org/xmlet/androidlayoutsapi"
 call mvn exec:java -D"exec.mainClass"="org.jetbrains.java.decompiler.main.decompiler.ConsoleDecompiler" -D"exec.args"="-dgs=true -log=WARN ./target/classes/org/xmlet/androidlayoutsapi ./src/main/java/org/xmlet/androidlayoutsapi"
 if exist "./target/classes/org" rmdir "./target/classes/org" /s /q
-ECHO END
